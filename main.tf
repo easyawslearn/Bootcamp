c3a1bc531bf1c04a5d290bda7de29b86a9b63f75
Vijay Patel <35554905+easyawslearn@users.noreply.github.com>
2022-08-22 12:19:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "ec2_jenkins" {
+  ami           = "ami-00dc79254d0461090"
+  instance_type = "t2.micro"
+ 
+  user_data = <<EOF
+		#! /bin/bash
+                sudo yum update -y
+		sudo yum install -y httpd.x86_64
+		sudo service httpd start
+		sudo service httpd enable
+		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+    yum install java-1.8.0-openjdk-devel -y
+    curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+    sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+    yum install -y jenkins
+    systemctl start jenkins
+    systemctl status jenkins
+    systemctl enable jenkins
+	   EOF
+
+  tags = {
+    Name = "Ec2-User-data"
+  }
+}

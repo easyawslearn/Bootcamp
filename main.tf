234dda649aa82cf0995f72570036931c65e79a60
Vijay Patel <35554905+easyawslearn@users.noreply.github.com>
2022-08-22 13:42:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.0"
+}
+
 resource "aws_instance" "ec2_jenkins" {
   ami           = "ami-00dc79254d0461090"
   instance_type = "t2.micro"
